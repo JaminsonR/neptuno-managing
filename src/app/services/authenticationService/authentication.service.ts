dfac606ecf0085a147948e2c303e3dd82f2ced93
Jaminson Riascos <jami.r.m@hotmail.com>
2018-10-18 23:20:42 -0500

    adding 'bearer' string back to auth token, chaging app ico

--- src/app/services/authenticationService/authentication.service.ts
+++ src/app/services/authenticationService/authentication.service.ts
@@ -50,7 +50,7 @@ export class AuthenticationService implements HttpInterceptor{
 		const token = localStorage.getItem('token');		
 		req = req.clone({
       setHeaders: {
-        Authorization: `${token}`
+        Authorization: `Bearer ${token}`
       }
 		});
 		return next.handle(req);
